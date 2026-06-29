dd8794ac21ed82758ec4a2b39e817f09971d65eb
Raj9556 <rajkumarsekar184@gmail.com>
2026-06-29 15:49:58 +0530

    add docker compose with postgresql

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(100))
 
 @app.route("/")
 def home():
-    return "DevOps pipeline is alive."
+    with app.app_context():
+        db.create_all()
+        visit = Visit(message="visited")
+        db.session.add(visit)
+        db.session.commit()
+        count = Visit.query.count()
+    return f"DevOps pipeline is alive. Total visits: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    
